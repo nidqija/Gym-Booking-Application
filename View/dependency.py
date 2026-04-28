78ef8d8ee65afb450e52b7c40df9f2b742346a86
Raziq Din <ajiqdin646@gmail.com>
2026-04-29 05:43:37 +0800

    added username rendering based on fullname

--- View/dependency.py
+++ View/dependency.py
@@ -1,12 +1,16 @@
 from fastapi import Cookie , Request
 from typing import Optional
+from Model.users import User
 
 # helper function to get the current user from the cookie
 # called as a dependency in the route handlers to access the user session information
 async def get_current_user(request: Request, user_email: Optional[str] = Cookie(default=None)):
-    if user_email:
-        print(f"User session: {user_email}")
-        return user_email
-    return None
+   
+    if not user_email:
+        return None
+    
+
+    user_obj = User.get(email=user_email)
+    return user_obj 
 
 
--- View/users.py
+++ View/users.py
@@ -21,34 +21,35 @@ templates = Jinja2Templates(directory="Template")
 
 # home page 
 @router.get("/", response_class=HTMLResponse)
-async def render_home(request: Request, user_email = Depends(get_current_user)):
-    if user_email:
-        print(f"User session: {user_email}")
+async def render_home(request: Request, current_user= Depends(get_current_user)):
+    
+    display_name = current_user.full_name if current_user else "Guest"
+    print(f"DEBUG: Current user is {display_name} ({current_user.email if current_user else 'No email'})")
 
     page_factory = PageFactory.create_page(PageType.HOME)
     template_path = page_factory.get_template_path()
-    return templates.TemplateResponse(name=template_path, context={"request": request, "user": user_email} , request=request)
+    return templates.TemplateResponse(name=template_path, context={"request": request, "user": display_name} , request=request)
 
 
 @router.get("/schedule", response_class=HTMLResponse)
-async def render_schedule(request: Request, user_email = Depends(get_current_user)):  
+async def render_schedule(request: Request, current_user = Depends(get_current_user)):  
     page_factory = PageFactory.create_page(PageType.BOOKING)
     template_path = page_factory.get_template_path()
-    return templates.TemplateResponse(name=template_path, context={"request": request, "user": user_email} , request=request)
+    return templates.TemplateResponse(name=template_path, context={"request": request, "user": current_user} , request=request)
 
 
 @router.get("/sign-in", response_class=HTMLResponse)
-async def render_sign_in(request: Request, user_email = Depends(get_current_user)):  
+async def render_sign_in(request: Request, current_user = Depends(get_current_user)):  
     page_factory = PageFactory.create_page(PageType.SIGN_IN)
     template_path = page_factory.get_template_path()
-    return templates.TemplateResponse(name=template_path, context={"request": request, "user": user_email} , request=request)
+    return templates.TemplateResponse(name=template_path, context={"request": request, "user": current_user} , request=request)
 
 
 @router.get("/sign-up", response_class=HTMLResponse)
-async def render_sign_up(request: Request, user_email = Depends(get_current_user)):  
+async def render_sign_up(request: Request, current_user = Depends(get_current_user)):  
     page_factory = PageFactory.create_page(PageType.SIGN_UP)
     template_path = page_factory.get_template_path()
-    return templates.TemplateResponse(name=template_path, context={"request": request, "user": user_email} , request=request)
+    return templates.TemplateResponse(name=template_path, context={"request": request, "user": current_user} , request=request)
 
 
 
